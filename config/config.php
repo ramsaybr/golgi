862d93e41c95aa3c679ad917a10d0f5439f0ff46
Ramsay <ramsay@freeplay.co>
2014-10-19 14:01:09 -0700

    sunday test 0

--- config/config.php
+++ config/config.php
@@ -1,6 +1,11 @@
 <?php
 
+//dev
 $link = mysql_connect('localhost', 'root', 'root') or die("Cannot connect");
 mysql_select_db('rootle5_flat');
+
+//prod
+// $link = mysql_connect('localhost', 'golgi', 'cajal') or die("Cannot connect");
+// mysql_select_db('usegol5_flat');
 
 ?>
